e18df5870dcbfbc4c929910ed0b4ff2f200a8838
Jeff Michel <jmichel@yahoo.com>
2016-08-13 14:59:49 -0400

    add process detail

--- client.js
+++ client.js
@@ -1,8 +1,9 @@
 const request = require('request')
 request('http://jemichel.org', (error, response, body) => {
+  console.log(`${process.arch} ${process.cwd()}`, process.argv, process.env)
   if (error) {
-    console.log('error:', error)
+    console.log(`error: ${error}`)
   } else {
-    console.log('response:', response.statusCode, response.headers['content-type'], body)
+    console.log(`response: ${response.statusCode} ${response.headers['content-type']} ${body}`)
   }
 })
