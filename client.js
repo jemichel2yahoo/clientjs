0768614a656d3139a59ca8da98e549d7a0f2b62c
Jeffrey Michel <jemichel@yahoo.com>
2016-08-20 14:20:17 -0400

    Make pretty

--- client.js
+++ client.js
@@ -1,4 +1,6 @@
+// Consume RESTful services!
 const request = require('request')
+
 request('http://jemichel.org', (error, response, body) => {
   console.log(`${process.arch} ${process.cwd()}`, process.argv, process.env)
   if (error) {
