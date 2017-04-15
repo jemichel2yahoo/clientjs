98e77dce1be9a76819644e21fb7ba1431ae0eef5
Jeffrey Michel <jemichel@yahoo.com>
2017-04-14 23:01:11 -0400

    Use options and ca.

--- client.js
+++ client.js
@@ -1,8 +1,9 @@
 // Consume RESTful services!!!
 
 const https = require('https')
+const fs = require('fs')
 
-https.get('https://api-sandbox.capitalone.com', (response) => {
+https.get({ hostname: 'api-sandbox.capitalone.com', ca: [ fs.readFileSync('verisign.pem') ] }, (response) => {
   console.log(`response: ${response.statusCode} ${response.headers['content-type']}`)
 
   response.on('data', (d) => {
