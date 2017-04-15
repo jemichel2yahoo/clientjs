4e9020fe3c610adaee986b8ac6327a8315e75344
Jeffrey Michel <jemichel@yahoo.com>
2017-04-15 12:39:38 -0400

    Use request module

--- client.js
+++ client.js
@@ -1,16 +1,15 @@
 // Consume RESTful services!!!
 
 const https = require('https')
+const request = require('request')
 const cas = require('ssl-root-cas/latest')
 
 https.globalAgent.options.ca = cas.create().addFile('verisign.pem')
 
-https.get('https://api-sandbox.capitalone.com', (response) => {
-  console.log(`response: ${response.statusCode} ${response.headers['content-type']}`)
-
-  response.on('data', (d) => {
-    console.log(`${d.toString('utf8')}`)
-  })
-}).on('error', (e) => {
-  console.error(e)
-});
+request.get('https://api-sandbox.capitalone.com', (error, response, body) => {
+  if (error) {
+    console.error(error)
+    return
+  }
+  console.log(`response: ${response.statusCode} ${response.headers['content-type']} ${body}`)
+})
