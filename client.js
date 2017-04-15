3539a6cecaf0213fa21fd79ccf38969219f4d1ac
Jeffrey Michel <jemichel@yahoo.com>
2017-04-15 10:44:56 -0400

    Support Node 4 and multiple CA certs

--- client.js
+++ client.js
@@ -3,11 +3,14 @@
 const https = require('https')
 const fs = require('fs')
 
-https.get({ hostname: 'api-sandbox.capitalone.com', ca: fs.readFileSync('verisign.pem', 'utf8') }, (response) => {
+https.get({
+    hostname: 'api-sandbox.capitalone.com',
+    ca: [ fs.readFileSync('c2.pem', 'utf8'), fs.readFileSync('c3.pem', 'utf8') ]
+  }, (response) => {
   console.log(`response: ${response.statusCode} ${response.headers['content-type']}`)
 
   response.on('data', (d) => {
-    console.log(`${new String(d)}`)
+    console.log(`${d.toString('utf8')}`)
   })
 }).on('error', (e) => {
   console.error(e)
