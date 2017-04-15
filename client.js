3143e818b9e61ed18c0cb3495825579dc972f5a9
Jeffrey Michel <jemichel@yahoo.com>
2017-04-14 22:37:49 -0400

    Use https module instead of request module.

--- client.js
+++ client.js
@@ -1,12 +1,13 @@
 // Consume RESTful services!!!
 
-const request = require('request')
+const https = require('https')
 
-request('https://api-sandbox.capitalone.com', (error, response, body) => {
-  // console.log(`${process.arch} ${process.cwd()}`, process.argv, process.env)
-  if (error) {
-    console.log(`error: ${error}`)
-  } else {
-    console.log(`response: ${response.statusCode} ${response.headers['content-type']} ${body}`)
-  }
-})
+https.get('https://api-sandbox.capitalone.com', (response) => {
+  console.log(`response: ${response.statusCode} ${response.headers['content-type']}`)
+
+  response.on('data', (d) => {
+    process.stdout.write(d)
+  })
+}).on('error', (e) => {
+  console.error(e)
+});
