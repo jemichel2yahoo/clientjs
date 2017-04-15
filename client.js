e2f2a716c637da2257bafa69e99ae667823a1fb3
Jeffrey Michel <jemichel@yahoo.com>
2017-04-15 10:51:31 -0400

    Use https.globalAgent to set CA

--- client.js
+++ client.js
@@ -3,10 +3,9 @@
 const https = require('https')
 const fs = require('fs')
 
-https.get({
-    hostname: 'api-sandbox.capitalone.com',
-    ca: [ fs.readFileSync('c2.pem', 'utf8'), fs.readFileSync('c3.pem', 'utf8') ]
-  }, (response) => {
+https.globalAgent.options.ca = [ fs.readFileSync('c2.pem', 'utf8'), fs.readFileSync('c3.pem', 'utf8') ]
+
+https.get('https://api-sandbox.capitalone.com', (response) => {
   console.log(`response: ${response.statusCode} ${response.headers['content-type']}`)
 
   response.on('data', (d) => {
