b66841f9b0f1a79bb3aa928258e110eb079f1314
Jeffrey Michel <jemichel@yahoo.com>
2017-04-22 14:57:45 -0400

    Use localhost.jemichel.org:4433 and its CA

--- client.js
+++ client.js
@@ -4,9 +4,9 @@ const https = require('https')
 const request = require('request')
 const cas = require('ssl-root-cas/latest')
 
-https.globalAgent.options.ca = cas.create().addFile('verisign.pem')
+https.globalAgent.options.ca = cas.create().addFile('cacert.pem')
 
-request.get('https://api-sandbox.capitalone.com', (error, response, body) => {
+request.get('https://localhost.jemichel.org:4433', (error, response, body) => {
   if (error) {
     console.error(error)
     return
