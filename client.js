cd8e11a2d9c7b4b99c283e4eb5be6bb75e180633
Jeffrey Michel <jemichel@yahoo.com>
2017-04-22 15:10:19 -0400

    Use request-promise

--- client.js
+++ client.js
@@ -1,15 +1,15 @@
 // Consume RESTful services!!!
 
 const https = require('https')
-const request = require('request')
+const rp = require('request-promise')
 const cas = require('ssl-root-cas/latest')
 
 https.globalAgent.options.ca = cas.create().addFile('cacert.pem')
 
-request.get('https://localhost.jemichel.org:4433', (error, response, body) => {
-  if (error) {
+rp.get('https://localhost.jemichel.org:4433')
+  .then((body) => {
+    console.log(`${body}`)
+  })
+  .catch((error) => {
     console.error(error)
-    return
-  }
-  console.log(`response: ${response.statusCode} ${response.headers['content-type']} ${body}`)
-})
+  })
